b5b2f1c78ecd1e516c361306d17c879211e78807
Oliver C <ocronk10@gmail.com>
2021-12-12 13:08:44 -0600

    Created divs for future dates to be used on scheudle as well as adjusted CSS for lock button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
+function generateHourBlock()
+{
+    $iBlock = $("<div>").addClass("row py-1");
 
+    $iTime = $("<div>").addClass("col-2 py-3 bg-warning").text("Hi");
+
+    $iText = $("<div>").addClass("col-8 py-3 bg-secondary").text("Hi");
+
+    $iLock = $("<div>").addClass("col-2 py-3 bg-warning").text("Hi");
+
+    $iBlock.append($iTime, $iText, $iLock);
+
+    $("#planner").append($iBlock);
+}
+
+generateHourBlock();
+generateHourBlock();
+generateHourBlock();
+generateHourBlock();
